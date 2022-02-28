7af18510ea5475b15816b993371939a60be9fef8
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-02-28 16:11:28 -0700

    Create Map, Filter, Reduce.py

---
+++ Map, Filter, Reduce.py
@@ -0,0 +1,10 @@
+#Map, Filter, Reduce
+
+#allow programmer to write simpler, shorter code without loops and branching
+#map and filter come built in with python (in __builtins__ module)
+#reduce has to be imported with functools
+
+#Map
+
+#map() has following syntax:
+#map(func,*iterables)
