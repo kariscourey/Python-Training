2cd228f2e93ab4c56ea07099c6f6955c4cd8079d
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-01 21:02:05 -0700

    Create ImportPrintHello.py

---
+++ ImportPrintHello.py
@@ -0,0 +1,17 @@
+#for import module practice
+
+# def __init__():
+#     print("hello")
+
+# def fun():
+#     print("hello")
+# if __name__ == "__main__":
+#     fun()
+
+def fun():
+    print("hello")
+fun()
+
+# def fun():
+#     print("hello")
+# __all__ = [fun]
